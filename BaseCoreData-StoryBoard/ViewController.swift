11f4379f7ab64b55cc2bafad3adfda7eac51e325
Chris Hand <cj.hand@gmail.com>
2022-08-23 21:49:22 -1000

    Initial Commit

---
+++ BaseCoreData-StoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseCoreData-StoryBoard
+//
+//  Created by Chris Hand on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
